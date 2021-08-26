b31c1f722b73de3590f6b08ed013b81ee8956f37
Luciano S. Santana <lucianosantanabr@gmail.com>
2021-08-26 15:28:25 -0300

    Update ReadLineWithStringOpenCsv.java

--- src/main/java/br/com/examplesjava/csv/ReadLineWithStringOpenCsv.java
+++ src/main/java/br/com/examplesjava/csv/ReadLineWithStringOpenCsv.java
@@ -1,1 +1,26 @@
-package br.com.examplesjava.csv;import com.opencsv.CSVReader;import com.opencsv.CSVReaderBuilder;import com.opencsv.exceptions.CsvException;import java.io.IOException;import java.io.Reader;import java.nio.file.Files;import java.nio.file.Paths;import java.util.List;public class ReadLineWithStringOpenCsv {  public static void main(String[] args) throws IOException, CsvException {    Reader reader = Files.newBufferedReader(Paths.get("people.csv"));    // skip header line => withSkipLines(1)    CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(0).build();    List<String[]> peoples = csvReader.readAll();    peoples.stream()        .forEach(            p -> System.out.println("Name : " + p[0] + " - Age : " + p[1] + " - Email : " + p[2]));  }}
+package br.com.examplesjava.csv;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadLineWithStringOpenCsv {
+
+  public static void main(String[] args) throws IOException, CsvException {
+
+    Reader reader = Files.newBufferedReader(Paths.get("people.csv"));
+
+    // skip header line => withSkipLines(1)
+    CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(0).build();
+
+    List<String[]> peoples = csvReader.readAll();
+    peoples.stream()
+        .forEach(
+            p -> System.out.println("Name : " + p[0] + " - Age : " + p[1] + " - Email : " + p[2]));
+  }
+}
