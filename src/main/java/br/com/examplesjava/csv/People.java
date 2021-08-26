6a94ac09019bc9c25881830457709ed938cfb5ee
Luciano S. Santana <lucianosantanabr@gmail.com>
2021-08-26 15:26:33 -0300

    Update People.java

--- src/main/java/br/com/examplesjava/csv/People.java
+++ src/main/java/br/com/examplesjava/csv/People.java
@@ -1,1 +1,19 @@
-package br.com.examplesjava.csv;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Getter;import lombok.Setter;@Getter@Setter@AllArgsConstructor@Builderpublic class People {  private String name;  private Integer age;  private String email;}
+package br.com.examplesjava.csv;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class People {
+
+  private String name;
+  private Integer age;
+  private String email;
+
+}
